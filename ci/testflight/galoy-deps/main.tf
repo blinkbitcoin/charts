e9ea2e6d79bc7f89d5ef2ea0ebc0cafb46811420
openoms <43343391+openoms@users.noreply.github.com>
2025-06-25 11:04:39 +0200

    fix: pin helm provider to 2.0 in all testflights (#7900)

--- ci/testflight/galoy-deps/main.tf
+++ ci/testflight/galoy-deps/main.tf
@@ -123,5 +123,9 @@ terraform {
       source  = "hashicorp/google"
       version = "4.64.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
--- ci/testflight/mempool/main.tf
+++ ci/testflight/mempool/main.tf
@@ -97,5 +97,9 @@ terraform {
       source  = "hashicorp/google"
       version = "4.64.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
